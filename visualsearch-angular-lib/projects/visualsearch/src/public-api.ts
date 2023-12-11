ad538a343129a08c715956401876192cff189aec
Balachandu Gillella <balachandu.gillella@sap.com>
2023-12-11 10:59:28 +0530

    Adding Visual Search Library code to the repo

---
+++ visualsearch-angular-lib/projects/visualsearch/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of visualsearch-angular-lib
+ */
+
+// export * from './lib/visualsearch-angular-lib.service';
+// export * from './lib/visualsearch-angular-lib.component';
+export * from './lib/visualsearch.module';
